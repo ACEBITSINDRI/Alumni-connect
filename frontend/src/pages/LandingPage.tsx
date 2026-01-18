62e8792c2d1c72f27758044b9942d095eff37006
kamleshthink <kamleshsamudih@gmail.com>
2026-01-18 07:19:28 +0530

    Fix JSX structure issues in LandingPage after removing stats section

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -48,15 +48,6 @@ const LandingPage: React.FC = () => {
     { src: lis, title: 'Night Views' },
   ];
 
-  const stats = {
-    alumni: 0,
-    students: 0,
-    companies: 0,
-    opportunities: 0,
-    events: 0,
-    projects: 0,
-  };
-
   const testimonials = [
     {
       id: 1,
@@ -248,8 +239,10 @@ const LandingPage: React.FC = () => {
                 Join as Alumni
               </Button>
             </div>
+          </div>
+        </div>
 
-            {/* Wave Separator */}
+        {/* Wave Separator */}
         <div className="absolute bottom-0 left-0 right-0">
           <svg viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 120L60 110C120 100 240 80 360 70C480 60 600 60 720 65C840 70 960 80 1080 85C1200 90 1320 90 1380 90L1440 90V120H1380C1320 120 1200 120 1080 120C960 120 840 120 720 120C600 120 480 120 360 120C240 120 120 120 60 120H0V120Z" fill="currentColor" className="text-white dark:text-gray-900"/>
