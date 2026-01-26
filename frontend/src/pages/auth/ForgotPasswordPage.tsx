a40f652f98f4e6f074d16473bd2a012f5f529243
KAMLESH KUMAR SHARMA <166900542+kamleshthink@users.noreply.github.com>
2026-01-26 19:53:15 +0530

    Update ForgotPasswordPage.tsx

--- frontend/src/pages/auth/ForgotPasswordPage.tsx
+++ frontend/src/pages/auth/ForgotPasswordPage.tsx
@@ -171,7 +171,7 @@ const ForgotPasswordPage: React.FC = () => {
         <div className="relative z-10">
           <div class="flex items-center space-x-4 mb-6">
             <div class="w-24 h-24 rounded-2xl flex items-center justify-center ">
-              <img alt="Alumni Connect Logo" class="w-full h-full object-contain" src="/assets/alumni_connect_logo-removebg-preview-B3U9VPj1.png">
+              <img alt="Alumni Connect Logo" class="w-full h-full object-contain" src="/assets/alumni_connect_logo-removebg-preview-B3U9VPj1.png"/>
             </div>
             <div>
               <h1 class="text-3xl font-bold text-white drop-shadow-lg">Alumni Connect</h1>
